312fd3c7c21adc87bdf5c73ab4277e44b76a7522
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-04-25 22:06:45 +0300

    Solved Tony Hawk Objects: object constructor

--- objects-tony-hawk-pro-skater/js/main.js
+++ objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,25 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function TonyHawkCharacter(chName, chStance, chSponsor, chMove) {
+  this.characterName = chName;
+  this.stance = chStance;
+  this.sponsor = chSponsor;
+  this.specialMove = chMove;
+
+  this.flip = function () {
+    console.log("Kickflip!!");
+  };
+  this.taunt = function () {
+    console.log(`Don't make me use my ${this.specialMove}`);
+  };
+  this.grab = function () {
+    console.log("Melon Grabbb");
+  };
+}
+
+let kareemCampbell = new TonyHawkCharacter(
+  "Karem",
+  "Goofy",
+  "Element",
+  "Ghetto Bird"
+);
