fadaaa44d352a89e84a5877931741ae405893d63
Kwame Obeng Yeboah <kwameobengyeboah@live.com>
2022-04-25 21:16:05 +0300

    completed lost galaxy

--- objects-lost-galaxy/js/main.js
+++ objects-lost-galaxy/js/main.js
@@ -1,1 +1,21 @@
 //Create a dog object that has four properties and three methods
+
+let dog = {};
+
+dog.breed = "Husky";
+dog.color = "brown";
+dog.age = 3;
+dog.legs = 4;
+dog.name = "Fido";
+
+dog.bark = function () {
+  console.log("WOOF");
+};
+
+dog.jump = function () {
+  console.log("BLAST OFF");
+};
+
+dog.fetch = function () {
+  console.log("ZOOOOOOMMMMMM");
+};
